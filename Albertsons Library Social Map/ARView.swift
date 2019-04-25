6b7ea39ec7714fc323c220fa929c6088ef9a850f
Multiplicison <46792534+Multiplicison@users.noreply.github.com>
2019-04-25 12:54:19 -0600

    Clear unworking starter app
    Deleted the starter app that didn't load the camera correctly.  Will replace with merging Chris and Connor's working models.

--- Albertsons Library Social Map/ARView.swift
+++
@@ -1,9 +0,0 @@
-//
-//  ARView.swift
-//  Albertsons Library Social Map
-//
-//  Created by Addison Jones on 4/18/19.
-//  Copyright © 2019 Addison Jones. All rights reserved.
-//
-
-import Foundation
--- README.md
+++
@@ -1,1 +0,0 @@
-# GIMM310_AlbertsonsLibrary
